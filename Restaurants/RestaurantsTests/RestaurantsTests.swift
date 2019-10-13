1c0fb7accede7f1f1c5e527a8e00169c39addcc0
Vimal Venugopalan <vimal.venugopalan@accenture.com>
2019-10-13 10:51:52 +0530

    Added Initial Project

---
+++ Restaurants/RestaurantsTests/RestaurantsTests.swift
@@ -0,0 +1,34 @@
+//
+//  RestaurantsTests.swift
+//  RestaurantsTests
+//
+//  Created by Venugopalan, Vimal on 13/10/19.
+//  Copyright © 2019 Venugopalan, Vimal. All rights reserved.
+//
+
+import XCTest
+@testable import Restaurants
+
+class RestaurantsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
